3e9ca2c11cad84dccb38ccfaea0ee3f9ca622f41
Karthikeyan <58913393+karthikeyan0312@users.noreply.github.com>
2021-08-10 20:41:24 +0530

    Add files via upload

---
+++ lang_det.py
@@ -0,0 +1,49 @@
+import pickle
+import streamlit as st
+from sklearn.feature_extraction.text import CountVectorizer 
+import pandas as pd
+import numpy as np
+import re  
+from sklearn.preprocessing import LabelEncoder 
+#from sklearn.feature_extraction.text import TfidfVectorizer as le
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB  #classifier is suitable for classification with discrete features
+
+st.title("Language Detection")
+path=r"E:\my python\language detection\Language Detection.csv"
+
+@st.cache
+def load_data():
+    data = pd.read_csv(path)
+    X = data["Text"]
+    y = data["Language"]
+    pickl=pd.read_pickle(r"E:\my python\language detection\lang_detection.pkl")
+
+    return (X,y,pickl)
+
+X,y,pkl=load_data()
+le=LabelEncoder()
+
+y = le.fit_transform(y)
+data_list = []
+# iterating through all the text
+for text in X:         
+    text = re.sub(r'[!@#$(),n"%^*?:;~`0-9]', ' ', text)      # removing the symbols and numbers
+    text = re.sub(r'[[]]', ' ', text)   
+    text = text.lower()          # converting the text to lower case
+    data_list.append(text)       # appending to data_list
+    # tokenize a collection of text documents
+cv=CountVectorizer()
+X = cv.fit_transform(data_list).toarray()
+
+
+def predicts(text):
+    x=cv.transform([text]).toarray()    
+    lang = pkl.predict(x)
+    lang = le.inverse_transform(lang)
+    return lang[0]
+
+t=st.text_input("Enter the a sentence ")
+submit=st.button("hit me")
+if submit:
+    st.write(predicts(t))    
